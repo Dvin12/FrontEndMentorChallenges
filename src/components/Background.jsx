759f3efaa61551a20c3b23c53696deab4b09f411
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-08-08 00:51:26 +0300

    Update Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,9 @@
 export default function Background() {
   return (
     <div className="h-full flex justify-end items-end w-full">
-      <div className=" w-screen md:h-[60%] h-screen  md:bg-bg-desktop bg-bg-mobile bg-cover"></div>
+      <div className=" w-screen md:h-[60%] h-screen  md:bg-bg-desktop bg-bg-mobile bg-cover">
+        A
+      </div>
     </div>
   );
 }
