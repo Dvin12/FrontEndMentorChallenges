472c2633d7bd04e69ff8b5c12f2f68f6f160a4a0
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-08-09 00:58:06 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
   return (
-    <section className="md:grid xl:grid-cols-4 xl:grid-rows-4 w-full h-full gap-8 xl:px-16 px-6 py-20 xl:py-12 md:grid-flow-row md:auto-cols-auto flex flex-col justify-center items-center xl:h-screen">
+    <section className="md:grid xl:grid-cols-4 xl:grid-rows-4 w-full h-full gap-8 xl:px-16 px-6 py-20 xl:py-12 md:grid-flow-row md:auto-cols-auto flex flex-col justify-center items-center xl:h-fit">
       {children}
     </section>
   );
