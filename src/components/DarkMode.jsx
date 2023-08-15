fdaffd6b5d4773f0def2b11b191336fb97680b48
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-08-15 20:04:30 +0300

    Update DarkMode.jsx

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -19,9 +19,7 @@ export default function DarkMode({ setTheme, theme }) {
         >
           <div
             className={`w-[28px] h-[28px] rounded-full bg-lightBg absolute top-1 ${
-              theme === "light"
-                ? "left-[36px]"
-                : `left-[5px] bg-darkCardBgHover `
+              theme === "light" ? "left-[36px]" : `left-[5px] bg-darkBg `
             } duration-300`}
           ></div>
         </div>
