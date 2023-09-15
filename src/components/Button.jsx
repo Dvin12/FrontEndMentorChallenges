de140e5c1258165d23214975ecad80b24b27157b
Dvin12 <edvinasvenclavovic@gmail.com>
2023-09-16 02:57:02 +0300

    started working on the desktop version, finished styling the nav and the header elements

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ children }) {
   return (
     <button
-      className={`px-4 py-3 rounded-md shadow-lg text-sm  font-medium ${
+      className={`px-4 py-3 xl:py-4 xl:px-6 rounded-md shadow-lg text-sm xl:text-base  font-medium ${
         children === "Get it on Chrome"
           ? "bg-sBlue text-[#fff] text-opacity-95"
           : " bg-gBlue bg-opacity-10 text-vDarkBlue"
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,19 @@ import MobileNav from "./MobileNav";
 
 export default function Navigation() {
   return (
-    <nav className="flex items-center justify-between px-8 py-10">
+    <nav className="flex items-center justify-between px-8 py-10 xl:px-44 xl:py-12">
       <img src="./assets/images/logo-bookmark.svg" alt="company name" />
+      <section className="items-center justify-center hidden text-sm font-medium tracking-widest gap-9 xl:flex text-vDarkBlue">
+        <a href="#">FEATURES</a>
+        <a href="#">PRICING</a>
+        <a href="#">CONTACT</a>
+        <a
+          href="#"
+          className="px-9 opacity-80 py-3 shadow-lg rounded-md bg-sRed text-[#fff]"
+        >
+          LOGIN
+        </a>
+      </section>
       <MobileNav />
     </nav>
   );
