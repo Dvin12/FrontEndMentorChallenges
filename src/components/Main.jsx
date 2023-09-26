89e82f0514d057e5d3932eb8b92516b336d393d0
Edvin Venclavovic <61581381+Dvin12@users.noreply.github.com>
2023-09-26 04:01:17 +0300

    Delete src directory

--- src/components/Main.jsx
+++
@@ -1,7 +0,0 @@
-export default function Main({ children }) {
-  return (
-    <main className="w-full h-full overflow-hidden font-openSans bg-background mix-blend-overlay">
-      {children}
-    </main>
-  );
-}
--- src/components/Section.jsx
+++
@@ -1,3 +0,0 @@
-export default function Section({ children }) {
-  return <section className="relative px-6">{children}</section>;
-}
