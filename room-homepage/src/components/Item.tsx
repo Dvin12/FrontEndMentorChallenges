aa26b5ef9b95b0b3d5217349c7986d41529f1d39
Dvin12 <edvinasvenclavovic@gmail.com>
2023-11-20 20:22:08 +0200

    added all the sections and finished mobile version

--- room-homepage/src/components/Item.tsx
+++ room-homepage/src/components/Item.tsx
@@ -6,12 +6,36 @@ interface ItemProps {
   slide: number;
 }
 
-export default function Item({ item, num, slide }: ItemProps): JSX.Element {
-  return slide === num ? (
-    <section>
-      <img src={item.imageMobile} alt="" />
-    </section>
-  ) : (
-    <></>
+export default function Item({
+  item: { imageDesktop, imageMobile, title, description },
+  num,
+  slide,
+}: ItemProps): JSX.Element {
+  return (
+    <>
+      {slide === num && (
+        <>
+          <section>
+            <img src={imageMobile} alt="" />
+          </section>
+          <section className="flex flex-col items-start justify-center gap-3 px-6 py-14">
+            <h1 className="text-4xl font-semibold tracking-tighter ">
+              {title}
+            </h1>
+            <p className=" text-darkGrey">{description}</p>
+            <button className="flex items-start gap-4 my-4">
+              <span className="tracking-[0.8em] text-lg font-semibold ">
+                SHOP NOW
+              </span>
+              <img
+                src="./assets/images/icon-arrow.svg"
+                alt=""
+                className="mt-1"
+              />
+            </button>
+          </section>
+        </>
+      )}
+    </>
   );
 }
--- room-homepage/src/components/Section.tsx
+++ room-homepage/src/components/Section.tsx
@@ -1,3 +1,25 @@
-export default function Section() {
-  return <div>Section</div>;
+export default function Section(): JSX.Element {
+  return (
+    <section>
+      <div>
+        <img src="./assets/images/image-about-dark.jpg" alt="" />
+      </div>
+      <div className="flex flex-col gap-4 px-6 py-14">
+        <h2 className=" font-semibold tracking-[0.3em]  ">
+          ABOUT OUR FURNITURE
+        </h2>
+        <p className=" text-darkGrey">
+          Our multifunctional collection blends design and function to suit your
+          individual taste. Make each room unique, or pick a cohesive theme that
+          best express your interests and what inspires you. Find the furniture
+          pieces you need, from traditional to contemporary styles or anything
+          in between. Product specialists are available to help you create your
+          dream space.
+        </p>
+      </div>
+      <div>
+        <img src="./assets/images/image-about-light.jpg" alt="" />
+      </div>
+    </section>
+  );
 }
