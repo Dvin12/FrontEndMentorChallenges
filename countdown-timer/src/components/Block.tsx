eb433bc6dd871a3e934b2615c79c86ad66e57313
Dvin12 <edvinasvenclavovic@gmail.com>
2023-11-23 15:10:23 +0200

    added logic, adjusted styling, added media queries

--- countdown-timer/src/components/Block.tsx
+++ countdown-timer/src/components/Block.tsx
@@ -1,31 +1,167 @@
-export default function Block(props: {
-  children: React.ReactNode;
-}): JSX.Element {
+import { useEffect, useState } from "react";
+
+interface BlockProps {
+  timerDays?: number;
+  timerHours?: number;
+  timerMinutes?: number;
+  timerSeconds?: number;
+}
+
+export default function Block({
+  timerDays,
+  timerHours,
+  timerMinutes,
+  timerSeconds,
+}: BlockProps): React.ReactElement {
+  const [prevDays, setPrevDays] = useState(timerDays);
+  const [prevHours, setPrevHours] = useState(timerHours);
+  const [prevMinutes, setPrevMinutes] = useState(timerMinutes);
+  const [prevSeconds, setPrevSeconds] = useState(timerSeconds);
+
+  const timeOver =
+    timerDays === 0 &&
+    timerHours == 0 &&
+    timerMinutes === 0 &&
+    timerSeconds === 0;
+
+  useEffect(() => {
+    if (prevDays !== timerDays) {
+      setTimeout(() => {
+        setPrevDays(timerDays);
+      }, 500);
+    }
+
+    if (prevHours !== timerHours) {
+      setTimeout(() => {
+        setPrevHours(timerHours);
+      }, 500);
+    }
+
+    if (prevMinutes !== timerMinutes) {
+      setTimeout(() => {
+        setPrevMinutes(timerMinutes);
+      }, 500);
+    }
+
+    if (prevSeconds !== timerSeconds) {
+      setTimeout(() => {
+        setPrevSeconds(timerSeconds);
+      }, 500);
+    }
+  }, [
+    timerDays,
+    timerHours,
+    timerMinutes,
+    timerSeconds,
+    prevDays,
+    prevHours,
+    prevMinutes,
+    prevSeconds,
+  ]);
+
   return (
-    <div className="time">
-      <div className="time-group">
-        <div className="time-segment">
-          <div className="segment-display">
-            <div className="segment-display-top">0</div>
-            <div className="segment-display-bottom">0</div>
-            <div className="segment-overlay">
-              <div className="segment-overlay-top">0</div>
-              <div className="segment-overlay-bottom">0</div>
+    <>
+      {timeOver ? (
+        <p>CONGRATULATIONS! YOU HAVE WASTED YOUR TIME FOR NOTHING! XD</p>
+      ) : (
+        <>
+          <div className="time">
+            <div className="time-group">
+              <div className="time-segment">
+                <div className="segment-display">
+                  <div className="dots">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                  </div>
+                  <div className="segment-display-top">{timerDays}</div>
+                  <div className="segment-display-bottom">{timerDays}</div>
+                  <div
+                    className={`segment-overlay ${
+                      timerDays !== prevDays && "flip"
+                    }`}
+                  >
+                    <div className="segment-overlay-top">{timerDays}</div>
+                    <div className="segment-overlay-bottom">{timerDays}</div>
+                  </div>
+                </div>
+              </div>
             </div>
+            <p>Days</p>
           </div>
-        </div>
-        <div className="time-segment">
-          <div className="segment-display">
-            <div className="segment-display-top">0</div>
-            <div className="segment-display-bottom">0</div>
-            <div className="segment-overlay">
-              <div className="segment-overlay-top">0</div>
-              <div className="segment-overlay-bottom">0</div>
+
+          <div className="time">
+            <div className="time-group">
+              <div className="time-segment">
+                <div className="segment-display">
+                  <div className="dots">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                  </div>
+                  <div className="segment-display-top">{timerHours}</div>
+                  <div className="segment-display-bottom">{timerHours}</div>
+                  <div
+                    className={`segment-overlay ${
+                      timerHours !== prevHours && "flip"
+                    }`}
+                  >
+                    <div className="segment-overlay-top">{timerHours}</div>
+                    <div className="segment-overlay-bottom">{timerHours}</div>
+                  </div>
+                </div>
+              </div>
             </div>
+            <p>Hours</p>
           </div>
-        </div>
-      </div>
-      <p>{props.children}</p>
-    </div>
+
+          <div className="time">
+            <div className="time-group">
+              <div className="time-segment">
+                <div className="segment-display">
+                  <div className="dots">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                  </div>
+                  <div className="segment-display-top">{timerMinutes}</div>
+                  <div className="segment-display-bottom">{timerMinutes}</div>
+                  <div
+                    className={`segment-overlay ${
+                      timerMinutes !== prevMinutes && "flip"
+                    }`}
+                  >
+                    <div className="segment-overlay-top">{timerMinutes}</div>
+                    <div className="segment-overlay-bottom">{timerMinutes}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p>Minutes</p>
+          </div>
+
+          <div className="time">
+            <div className="time-group">
+              <div className="time-segment">
+                <div className="segment-display">
+                  <div className="dots">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                  </div>
+                  <div className="segment-display-top">{timerSeconds}</div>
+                  <div className="segment-display-bottom">{timerSeconds}</div>
+                  <div
+                    className={`segment-overlay ${
+                      timerSeconds !== prevSeconds && "flip"
+                    }`}
+                  >
+                    <div className="segment-overlay-top">{timerSeconds}</div>
+                    <div className="segment-overlay-bottom">{timerSeconds}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p>Seconds</p>
+          </div>
+        </>
+      )}
+    </>
   );
 }
--- countdown-timer/src/components/Countdown.tsx
+++ countdown-timer/src/components/Countdown.tsx
@@ -1,13 +1,58 @@
+import { useEffect, useRef, useState } from "react";
 import Block from "./Block";
 import "./countdown.css";
 
 export default function Countdown(): JSX.Element {
+  const [timerDays, setTimerDays] = useState(1);
+  const [timerHours, setTimerHours] = useState(1);
+  const [timerMinutes, setTimerMinutes] = useState(1);
+  const [timerSeconds, setTimerSeconds] = useState(1);
+
+  const interval = useRef<number>();
+
+  function startTimer() {
+    const countdownDate = new Date("May 24, 2024 00:00:00").getTime();
+
+    interval.current = window.setInterval(() => {
+      const present = new Date().getTime();
+      const distance = countdownDate - present;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  }
+
+  useEffect(() => {
+    startTimer();
+
+    return () => {
+      if (interval.current) {
+        clearInterval(interval.current);
+      }
+    };
+  });
+
   return (
     <section className="countdown">
-      <Block>Days</Block>
-      <Block>Hours</Block>
-      <Block>Minutes</Block>
-      <Block>Seconds</Block>
+      <Block
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes}
+        timerSeconds={timerSeconds}
+      />
     </section>
   );
 }
