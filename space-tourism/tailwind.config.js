9251f1419621acada2452be846477513f74d2c83
Dvin12 <edvinasvenclavovic@gmail.com>
2023-11-24 13:54:07 +0200

    set up the project

---
+++ space-tourism/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      bellefair: ["Bellefair"],
+      barlowCondensed: ["Barlow-Condensed"],
+    },
+    colors: {
+      darkBlue: "#0B0D17",
+      skyBlue: "#D0D6F9",
+      white: "#FFFFFF",
+      transparent: "transparent",
+    },
+  },
+  plugins: [],
+};
